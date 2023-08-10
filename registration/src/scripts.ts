0b8951215c69eaac893eedcabd4876d6ab982ee5
Jānis Kaveckis <kaveckisj@gmail.com>
2023-08-10 14:52:02 +0300

    Start of the scripting with axios

--- registration/src/scripts.ts
+++ registration/src/scripts.ts
@@ -1,8 +1,1 @@
-import $ from 'jquery';
-import sum from './utils/sum/sum';
-
-console.log('Ready for coding');
-
-console.log('Body jQuery node:', $('body'));
-console.log('Body javascript node:', document.querySelector('body'));
-console.log('2 + 3 =', sum(2, 3));
+import axios from "axios";
