98bfc6e2296c803dd7762a5c5f17e0105c61fbf9
Jānis Kaveckis <kaveckisj@gmail.com>
2023-08-11 19:57:09 +0300

    Added js script to save and get data from js-server db, corrected some mistakes and added padding for .main class

--- registration/src/scripts.ts
+++ registration/src/scripts.ts
@@ -1,1 +1,59 @@
 import axios from "axios";
+
+const form = document.querySelector<HTMLFormElement>('.form');
+const posts = document.querySelector('.js-posts');
+const submitButton = document.querySelector<HTMLButtonElement>('.js-submit')
+
+type Post = {
+    fname: string,
+    lname: string,
+    email: string,
+    gender: string,
+    country: string,
+}
+
+const addPostsHTML = (post: Post) => {
+    const postHTML = `
+        <div>
+        <h1>${post.fname} ${post.lname}</h1>
+        <p>${post.email}</p>
+        <p>${post.gender}</p>
+        <p>${post.country}</p>
+        </div>
+        `;
+
+        posts.innerHTML += postHTML;
+}
+
+axios.get('http://localhost:3004/posts').then((res) =>{
+    console.log(res.data)
+
+    res.data.forEach((post: Post) => {
+        addPostsHTML(post);
+    });
+})
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    submitButton.disabled = true;
+
+    const formData = new FormData(form);
+
+    const finalData: {[key: string]: unknown} = {};
+
+    for (const pair of formData.entries()) {
+        finalData[pair[0]] = pair[1];
+    }
+
+    axios.post('http://localhost:3004/posts', finalData).then((res) => {
+        console.log('success', res.data);
+        const post: Post = res.data;
+
+        addPostsHTML(post)
+
+        form.reset()
+        submitButton.disabled = false;
+    })
+
+    console.log(finalData)
+})
