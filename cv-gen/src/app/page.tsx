1f5c8e3d476560542d8f3606107ffa9377f3916e
Jānis Kaveckis <kaveckisj@gmail.com>
2023-08-27 00:46:41 +0300

    Made only one side of the page scrollable

--- cv-gen/src/app/page.tsx
+++ cv-gen/src/app/page.tsx
@@ -122,108 +122,110 @@ function createDescriptionField(header: string,
   }
 
   return (
-    <main className={styles.main}>
-      <div className={styles.input}>
-        <div className={styles.info}>
-            {createInputField('all_line', 'Name', 'text', 'full_name', 'John Doe', 'one_line')}
-            {createInputField('all_line', 'Objective', 'text', 'objective', 'Software Developer', 'one_line')}
-          <div className={styles.inline}>
-            {createInputField('biggest', 'Email', 'email', 'email', 'example@example.org')}
-            {createInputField('', 'Phone', 'number', 'phone', 'Phone number...')}
+    <body className={styles.body}>
+      <main className={styles.main}>
+        <div className={styles.input}>
+          <div className={styles.info}>
+              {createInputField('all_line', 'Name', 'text', 'full_name', 'John Doe', 'one_line')}
+              {createInputField('all_line', 'Objective', 'text', 'objective', 'Software Developer', 'one_line')}
+            <div className={styles.inline}>
+              {createInputField('biggest', 'Email', 'email', 'email', 'example@example.org')}
+              {createInputField('', 'Phone', 'number', 'phone', 'Phone number...')}
+            </div>
+            <div className={styles.inline}>
+              {createInputField('biggest', 'Website', 'text', 'website', 'Your website...')}
+              {createInputField('', 'Location', 'text', 'location', 'Your location...')}
+            </div>
           </div>
-          <div className={styles.inline}>
-            {createInputField('biggest', 'Website', 'text', 'website', 'Your website...')}
-            {createInputField('', 'Location', 'text', 'location', 'Your location...')}
+          <div className={styles.info}>
+            <h1>Work experience</h1>
+            {createInputField('all_line', 'Company', 'text', 'company', 'Company name...', 'one_line')}
+            <div className={styles.inline}>
+              {createInputField('biggest', 'Job title', 'text', 'job_title', 'Software Developer')}
+              {createInputField('', 'Date from', 'date', 'date_job_from')}
+              {createInputField('', 'Date to', 'date', 'date_job_to')}
+            </div>
+            {createDescriptionField('Description', 'job_description')}
           </div>
+          <div className={styles.info}>
+            <h1>Education</h1>
+            <div className={styles.inline}>
+              {createInputField('biggest', 'School', 'text', 'school_name', 'School name...')}
+              {createInputField('', 'Date from', 'date', 'date_school_from')}
+              {createInputField('', 'Date to', 'date', 'date_school_to')}
+            </div>
+            <div className={styles.inline}>
+            {createInputField('biggest', 'Degree', 'text', 'degree', 'High school degree')}
+            {createInputField('', 'GPA', 'number', 'gpa', '3.81')}
+            </div>
+            {createDescriptionField('Description', 'school_description')}
+          </div>
+          <div className={styles.info}>
+            <h1>Skills</h1>
+            {createDescriptionField('Skills list', 'skills_list')}
+          </div>
+          <pre>{JSON.stringify(inputData, null, 2)}</pre>
         </div>
-        <div className={styles.info}>
-          <h1>Work experience</h1>
-          {createInputField('all_line', 'Company', 'text', 'company', 'Company name...', 'one_line')}
-          <div className={styles.inline}>
-            {createInputField('biggest', 'Job title', 'text', 'job_title', 'Software Developer')}
-            {createInputField('', 'Date from', 'date', 'date_job_from')}
-            {createInputField('', 'Date to', 'date', 'date_job_to')}
-          </div>
-          {createDescriptionField('Description', 'job_description')}
-        </div>
-        <div className={styles.info}>
-          <h1>Education</h1>
-          <div className={styles.inline}>
-            {createInputField('biggest', 'School', 'text', 'school_name', 'School name...')}
-            {createInputField('', 'Date from', 'date', 'date_school_from')}
-            {createInputField('', 'Date to', 'date', 'date_school_to')}
-          </div>
-          <div className={styles.inline}>
-          {createInputField('biggest', 'Degree', 'text', 'degree', 'High school degree')}
-          {createInputField('', 'GPA', 'number', 'gpa', '3.81')}
-          </div>
-          {createDescriptionField('Description', 'school_description')}
-        </div>
-        <div className={styles.info}>
-          <h1>Skills</h1>
-          {createDescriptionField('Skills list', 'skills_list')}
-        </div>
-        <pre>{JSON.stringify(inputData, null, 2)}</pre>
-      </div>
-      
-      <div className={styles.output} id='output'>
-      <iframe
-          srcDoc={`<!DOCTYPE html>
-            <html>
-              <head>
-              <style>
-              .inline {
-                display: flex;
-                flex-direction: row;
-                padding: 10px;
-                justify-content: space-between;
-                gap: 10px;
-              }
-              .from_to {
-                display: flex;
-                flex-direction: row;
-                padding: 10px;
-                gap: 3px;
-                justify-content: flex-start;
-              }
+        
+        <div className={styles.output} id='output'>
+        <iframe
+            srcDoc={`<!DOCTYPE html>
+              <html>
+                <head>
+                <style>
+                .inline {
+                  display: flex;
+                  flex-direction: row;
+                  padding: 10px;
+                  justify-content: space-between;
+                  gap: 10px;
+                }
+                .from_to {
+                  display: flex;
+                  flex-direction: row;
+                  padding: 10px;
+                  gap: 3px;
+                  justify-content: flex-start;
+                }
 
-              .first_style {
-                background-color: white;
-              }
-            </style>
-              </head>
-              <body class='first_style'>
-                <h1>${inputData.full_name}</h1>
-                <p>${inputData.objective}</p>
-                <div class='inline'>
-                  ${fieldChanged.email ? `<p>Email: ${inputData.email}</p>` : ''}
-                  ${fieldChanged.phone ? `<p>Phone: ${inputData.phone}</p>` : ''}
-                  ${fieldChanged.website ? `<p>Website: ${inputData.website}</p>` : ''}
-                  ${fieldChanged.location ? `<p>Location: ${inputData.location}</p>` : ''}
-                </div>
-                <h3>Work experience</h3>
-                <div>
-                  ${fieldChanged.company ? `<h4>Company: ${inputData.company}<h4>` : ''}
-                  ${fieldChanged.job_title ? `<p>Job title: ${inputData.job_title}</p>` : ''}
-                  ${fieldChanged.date_job_from ? `<div class='from_to'><p>From ${inputData.date_job_from.toString()} to ${fieldChanged.date_job_to ? `<p>${inputData.date_job_to.toString()}` : ''}</p></div>` : ''}
-                  ${fieldChanged.job_description ? `<h4>Job Description:</h4><ul>${inputData.job_description}</ul>` : ''}
-                </div>
-                <h3>Education</h3>
-                <div>
-                  ${fieldChanged.school_name ? `<h4>School name: ${inputData.school_name}<h4>` : ''}
-                  ${fieldChanged.date_school_from ? `<div class='from_to'><p>From ${inputData.date_school_from.toString()} to ${fieldChanged.date_school_to ? `<p>${inputData.date_school_to.toString()}` : ''}</p></div>` : ''}
-                  ${fieldChanged.degree ? `<div class='from_to'><p>${inputData.degree} - ${fieldChanged.gpa ? `<p>${inputData.gpa}` : ''}</p></div>` : ''}
-                  ${fieldChanged.school_description ? `<h4>School Description:</h4><ul>${inputData.school_description}</ul>` : ''}
-                </div>
-                <h3>Skills</h3>
-                <div>
-                  ${fieldChanged.skills_list ? `<h4>Skills List:</h4><ul>${inputData.skills_list}</ul>` : ''}
-                </div>
-              </body>
-            </html>`}
-          style={{ width: '100%', height: '100%' }}
-        />
-      </div>
-    </main>
+                .first_style {
+                  background-color: white;
+                }
+              </style>
+                </head>
+                <body class='first_style'>
+                  <h1>${inputData.full_name}</h1>
+                  <p>${inputData.objective}</p>
+                  <div class='inline'>
+                    ${fieldChanged.email ? `<p>Email: ${inputData.email}</p>` : ''}
+                    ${fieldChanged.phone ? `<p>Phone: ${inputData.phone}</p>` : ''}
+                    ${fieldChanged.website ? `<p>Website: ${inputData.website}</p>` : ''}
+                    ${fieldChanged.location ? `<p>Location: ${inputData.location}</p>` : ''}
+                  </div>
+                  <h3>Work experience</h3>
+                  <div>
+                    ${fieldChanged.company ? `<h4>Company: ${inputData.company}<h4>` : ''}
+                    ${fieldChanged.job_title ? `<p>Job title: ${inputData.job_title}</p>` : ''}
+                    ${fieldChanged.date_job_from ? `<div class='from_to'><p>From ${inputData.date_job_from.toString()} to ${fieldChanged.date_job_to ? `<p>${inputData.date_job_to.toString()}` : ''}</p></div>` : ''}
+                    ${fieldChanged.job_description ? `<h4>Job Description:</h4><ul>${inputData.job_description}</ul>` : ''}
+                  </div>
+                  <h3>Education</h3>
+                  <div>
+                    ${fieldChanged.school_name ? `<h4>School name: ${inputData.school_name}<h4>` : ''}
+                    ${fieldChanged.date_school_from ? `<div class='from_to'><p>From ${inputData.date_school_from.toString()} to ${fieldChanged.date_school_to ? `<p>${inputData.date_school_to.toString()}` : ''}</p></div>` : ''}
+                    ${fieldChanged.degree ? `<div class='from_to'><p>${inputData.degree} - ${fieldChanged.gpa ? `<p>${inputData.gpa}` : ''}</p></div>` : ''}
+                    ${fieldChanged.school_description ? `<h4>School Description:</h4><ul>${inputData.school_description}</ul>` : ''}
+                  </div>
+                  <h3>Skills</h3>
+                  <div>
+                    ${fieldChanged.skills_list ? `<h4>Skills List:</h4><ul>${inputData.skills_list}</ul>` : ''}
+                  </div>
+                </body>
+              </html>`}
+            style={{ width: '100%', height: '100%' }}
+          />
+        </div>
+      </main>
+    </body>
   )
 }
